e2e5c7b5c79287236f35558728a23b7bdc3322fa
Jashmae Baculpo <jbaculpo@csumb.edu>
2018-05-18 02:42:52 +0000

    Add beginning crap

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <link rel="stylesheet" href="styles.css" type="text/css" />
+    <head>
+        <title>
+            Shattered Divinations
+        </title>
+    </head>
+    
+    <body>
+        <div id = nameOfWebsite>
+        <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">            
+        <h1>
+            Shattered Divinations
+            </h1>
+        </div>
+        
+        
+        
+    </body>
+</html>
