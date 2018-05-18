30c13d12e18f19d01473ce9385cd9dfdffd142b0
Jashmae Baculpo <jbaculpo@csumb.edu>
2018-05-18 18:53:12 +0000

    added photos of crystals and got the admin hyperlink to somewhat work

--- index.php
+++ index.php
@@ -4,10 +4,13 @@
 
 <!DOCTYPE html>
 <html>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     <link rel="stylesheet" href="styles.css" type="text/css" />
     <head>
         <title>
-            Shattered Divinations
+            Rock of the Month
         </title>
     </head>
     
@@ -15,11 +18,18 @@
         <div id = nameOfWebsite>
         <link href="https://fonts.googleapis.com/css?family=Bad+Script" rel="stylesheet">            
         <h1>
-            Shattered Divinations
+            Rock of the Month
             </h1>
         </div>
         
+        <dv id = "admin">
+            <a href= "login.php">Admin?</a>
+        </dv>
         
+        <?php
+            $imgURL = "./Crystals/Amethyst.jpg";
+            echo "<img src = '$imgURL' >";
+        ?>
         
     </body>
 </html>
---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <h1>Login</h1>
+        <h2>Credentials required before submiting form.</h2>
+        <p>You can log in using usernames <strong>admin1</strong> or <strong>admin2</strong>. The password is <strong>hello</strong>.</p>
+        
+        <!--Form to enter credentials-->
+        <form method = "post" action = "verifyUser.php">
+            <input type="text" name="username" placeholder = "Username"/><br/>
+            <input type="text" name="password" placeholder = "Password"/><br/>
+            <input type= "submit"  name= "submit" value= "Login"/>
+        </form>
+        
+    </body>
+</html>
