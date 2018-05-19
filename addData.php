3860023ffa1835664f3ab75997e833b020fb24ae
Jashmae Baculpo <jbaculpo@csumb.edu>
2018-05-19 05:05:26 +0000

    I am freezing as I type this

--- addData.php
+++ addData.php
@@ -6,16 +6,13 @@
     //Checking credentials in database
     $sql = "INSERT INTO crystals (gemID, name, habitat, color, luster, element, price, mohs)
             VALUES (:NULL, :name, :habitat, :color, :luster, :element, :price, :mohs)";
-            echo "made the crystal";
+    echo "made the crystal";
     $stmt = $connect->prepare($sql);
-
+    echo "made the statement \n";
     $data = array(":name" => $_POST['name'], ":habitat" => $_POST['habitat'], ":color" => $_POST['color'], ":luster" => $_POST['luster'], ":element" => $_POST['element'], 
                     ":price" => $_POST['price'], ":mohs" => $_POST['mohs']);
    
     $dbConn->exec($sql);
-
-    $result = $stmt->fetch(PDO::FETCH_ASSOC);
-    
-   
+    echo "connected\n";
 ?>
 
