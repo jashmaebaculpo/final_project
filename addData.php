679be4fb784f509a0aa744fee00b6ec08d87cba6
Jashmae Baculpo <jbaculpo@csumb.edu>
2018-05-19 05:13:48 +0000

    still cold AND my feet are dead

--- addData.php
+++ addData.php
@@ -6,13 +6,14 @@
     //Checking credentials in database
     $sql = "INSERT INTO crystals (gemID, name, habitat, color, luster, element, price, mohs)
             VALUES (:NULL, :name, :habitat, :color, :luster, :element, :price, :mohs)";
-    echo "made the crystal";
     $stmt = $connect->prepare($sql);
-    echo "made the statement \n";
-    //$data = array(":name" => $_POST['name'], ":habitat" => $_POST['habitat'], ":color" => $_POST['color'], ":luster" => $_POST['luster'], ":element" => $_POST['element'], 
-                    //":price" => $_POST['price'], ":mohs" => $_POST['mohs']);
-   echo "Made the data\n";
-    $dbConn->exec($sql);
-    echo "connected\n";
+    $data = array(":name" => $_POST['name'], ":habitat" => $_POST['habitat'], ":color" => $_POST['color'], ":luster" => $_POST['luster'], ":element" => $_POST['element'], 
+                    ":price" => $_POST['price'], ":mohs" => $_POST['mohs']);
+   
+    $stmt->execute($data);
+    
+    if( $stmt === false ) {
+        var_dump(sqlsrv_errors);die;
+    }
 ?>
 
